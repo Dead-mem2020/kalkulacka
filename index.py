26e9d437678195080d9ced048b6e09506d38449b
Stanislav Roll <Deadmem2000@gmail.com>
2023-12-12 09:30:30 +0100

    hotový úkol

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 print("Skvělý den!")
-x = input("Abychom začali,zadejte proměnou x: ")
+x = input("Abychom začali,zadejte proměnou x: ") #input = vstup(ptá se uživatele,co má do x vložit)
 y = input("Teď zadejte proměnou y: ")
 
+#přetypovává na integer(číslo)
 x = int(x)
 y = int(y)
 
@@ -10,18 +11,21 @@ print(" Pro sčítání +")
 print(" Pro odčítání -")
 print(" Pro násobení *")
 print(" Pro dělení /")
-print(" Pro mocnění **, (x = mocněnec a y = mocnitel)")
+print(" Pro mocnění **, (x = mocněnec a y = mocnitel)")                             #komentáře (hashtagy) se dělají pravým altem a x
 print(" A pro odmocnění /*, (x = odmocninec a y dmocnitel)")
 
 znamenko = input("Zadejte vaší volbu operátoru: ")
 
+#podmínky se dělají oříkazem if
 if znamenko == "+":
   print(x+y)
   print("Du bist sehr tolle,Freund :D (to nebudu překládat)")
 elif znamenko == "-":
   print(x-y)
   print("Du bist sehr tolle,Freund :D (to nebudu překládat)")
+#pokud chci v podmínce pokračovat, nedávám if, ale elif
 elif znamenko == "/":
+  #jestliže chci dále něco kontrolovat, mohu podmínky vnořovat jako tady if
   if y == 0:
     print("TY KOKOS NÉÉÉÉÉ")
   else:
